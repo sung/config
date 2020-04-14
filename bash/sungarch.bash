3281a48cb1c787112090ca1c06dcc90e55a338f6
Sung Gong <sung@bio.cc>
2020-04-14 15:55:27 +0100

    bash & tmux for sungarch

---
+++ bash/sungarch.bash
@@ -0,0 +1,17 @@
+#################
+# sung's bashrc #
+#################
+# VIM first 
+export EDITOR=vim
+#setterm -blength 0
+set -o vi
+
+######################################
+# User specific aliases and functions #
+#######################################
+alias ls='ls -F --color=auto'
+alias ll='ls -l'
+alias grep='grep --color=auto'
+alias vi='vim'
+alias ssh='ssh -XC'
+
